b1918f5e0c30a64d020047f46076c23de1e3e0ce
ghrsmg <gherasimgabriel309@gmail.com>
2024-11-19 13:28:48 +0200

    RestAssured!

--- test/petStore/PetTest.java
+++ test/petStore/PetTest.java
@@ -1,15 +1,21 @@
 package petStore;
 
-
+import com.google.gson.Gson;
 import io.restassured.http.*;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import juiceShop.frameworkUtils.Utils;
 
+import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import petStore.models.Category;
+import petStore.models.Pet;
+import petStore.models.Tag;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -17,78 +23,65 @@ import java.util.Iterator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
 public class PetTest extends BaseTest{
 
     @DataProvider(name = "FindPetById")
     public Iterator<Object[]> findPetDp () {
         Collection<Object[]> dp = new ArrayList<>();
-      //  dp.add(new String[] {"100","200"});
-        dp.add(new String[] {"9999","404",""});
-        dp.add(new String[] {"10","200","doggie"});
+        dp.add(new String[] {"9999", "404", ""});
+        dp.add(new String[] {"10", "200", "doggie"});
         return dp.iterator();
     }
 
     @Test(dataProvider = "FindPetById")
-    public void findPetById(String petId, String responseCode,String name){
+    public void findPetById(String petId, String responseCode, String name) {
 
-        Response response =  httpRequest.request(Method.GET,"/pet/{petId}", petId);
-        Assert.assertEquals(response.getStatusCode(),Integer.parseInt(responseCode));
+        Response response = httpRequest.request(Method.GET, "/pet/" + petId);
+        Assert.assertEquals(response.getStatusCode(), Integer.parseInt(responseCode));
         assertThat(response.getStatusLine(), Matchers.containsStringIgnoringCase("HTTP/1.1"));
-        Assert.assertEquals(response.getHeader("Content-Type"),"application/json");
-        Assert.assertEquals(response.getHeader("Access-Control-Allow-Methods"),"GET, POST, DELETE, PUT");
-        Assert.assertEquals(response.getDetailedCookies().asList().size(),0);
-        System.out.println(response.getBody().asString());
+        Assert.assertEquals(response.getHeader("Content-Type"), "application/json");
+        Assert.assertEquals(response.getHeader("Access-Control-Allow-Methods"), "GET, POST, DELETE, PUT");
+        Assert.assertEquals(response.getDetailedCookies().asList().size(), 0);
+
+//        System.out.println(response.getBody().asString());
         JsonPath jsonPathEvaluator = response.jsonPath();
-//        System.out.println(jsonPathEvaluator.get("name").toString());
-        if(response.getStatusCode()==404){
-            Assert.assertEquals(jsonPathEvaluator.getString("message"),"Pet not found");
+        if(response.getStatusCode() == 404) {
+            Assert.assertEquals(jsonPathEvaluator.getString("message"), "Pet not found");
         }
-        if(response.getStatusCode()==200){
-            Assert.assertEquals(jsonPathEvaluator.getString("name"),name);
-      //      Assert.assertEquals(jsonPathEvaluator.getString("category.name"),"string");
-
+        if (response.getStatusCode() == 200) {
+            Assert.assertEquals(jsonPathEvaluator.getString("name"), name);
+//            Assert.assertEquals(jsonPathEvaluator.getString("category.name"), "string");
         }
-
-
-//        Cookies cookies = response.getDetailedCookies();
-//        System.out.println("Found cookies: "+ cookies.asList().size());
-//        for(Cookie c : cookies){
-//            System.out.println(c.getName() + ": " + c.getValue());
-//        }
-//
-//        Headers headers = response.getHeaders();
-//        for (Header h:headers)
-//              {
-//                  System.out.println(h.getName()+": " + h.getValue());
-//        }
-//        System.out.println(response.prettyPrint());
-
     }
+
     @Test
-    public void createPet(){
-        httpRequest.body("{\"{\\n\" +\n" +
-                "                \"  \\\"id\\\": 0,\\n\" +\n" +
-                "                \"  \\\"category\\\": {\\n\" +\n" +
-                "                \"    \\\"id\\\": 0,\\n\" +\n" +
-                "                \"    \\\"name\\\": \\\"string\\\"\\n\" +\n" +
-                "                \"  },\\n\" +\n" +
-                "                \"  \\\"name\\\": \\\"doggie\\\",\\n\" +\n" +
-                "                \"  \\\"photoUrls\\\": [\\n\" +\n" +
-                "                \"    \\\"string\\\"\\n\" +\n" +
-                "                \"  ],\\n\" +\n" +
-                "                \"  \\\"tags\\\": [\\n\" +\n" +
-                "                \"    {\\n\" +\n" +
-                "                \"      \\\"id\\\": 0,\\n\" +\n" +
-                "                \"      \\\"name\\\": \\\"}");
-        Response response =  httpRequest.request(Method.GET,"/pet");
+    public void createPet() {
+        Category cat = new Category(231, "myName");
+        Tag tag = new Tag(567, "tagName");
+        ArrayList<Tag> tags = new ArrayList<>();
+        tags.add(tag);
+        ArrayList<String> photoUrls = new ArrayList<>();
+        photoUrls.add("http://myurl.com");
+        Pet pet = new Pet(699, cat, "bobitza", photoUrls, tags, "available");
+
+        // Serilizing Object to JSON String
+        Gson gson = new Gson();
+        String jsonOutput = gson.toJson(pet);
+        System.out.println(jsonOutput);
+
+        httpRequest.body(jsonOutput);
+        Response response = httpRequest.request(Method.POST, "/pet");
         System.out.println(response.getBody().asString());
         System.out.println(response.getStatusCode());
 
-
+        // Deserlizing Json string to Object
+        Pet myPet = gson.fromJson(jsonOutput, Pet.class);
+        System.out.println(myPet.getName());
     }
+
     @Test
-    public void createPet2(){
+    public void createPet2() {
+
         JSONObject requestParams = new JSONObject();
         requestParams.put("id", "100");
         requestParams.put("name", "doggie");
@@ -96,10 +89,7 @@ public class PetTest extends BaseTest{
         httpRequest.body(requestParams.toString());
         Response response = httpRequest.request(Method.POST, "/pet");
         System.out.println(response.getBody().asString());
-
-
+        System.out.println(response.getStatusCode());
     }
-
-
 
 }
--- test/petStore/models/Tag.java
+++ test/petStore/models/Tag.java
@@ -1,4 +1,12 @@
 package petStore.models;
 
-public class Tag {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Tag{
+    private int id;
+    private String name;
 }
